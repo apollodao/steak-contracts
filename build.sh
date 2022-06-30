9894907fd4cc7ec739a8f649b6388e3c1b3312d2
George McAskill <42020095+georgemc98@users.noreply.github.com>
2022-06-30 17:38:01 -0500

    feat: wip integrating proto messages

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+set -e
+set -u
+
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found"
+fi
+
 docker run -e GIT_CREDENTIALS --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
