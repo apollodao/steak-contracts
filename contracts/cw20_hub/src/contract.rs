e68bc9ace8e6fbfdea2fa6c1befa2d4ac8cec767
Pacman <pacman@apollo.farm>
2022-07-05 15:03:22 +0200

    feat: copy osmosis hub into cw20 hub

---
+++ contracts/cw20_hub/src/contract.rs
@@ -0,0 +1,42 @@
+use cosmwasm_std::{
+    entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult,
+};
+use cw_asset::osmosis::OsmosisCoin;
+use steak::error::ContractError;
+use steak::execute;
+use steak::hub::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    execute::instantiate::<OsmosisCoin>(deps, env, msg)
+}
+
+#[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    execute::execute::<OsmosisCoin>(deps, env, info, msg)
+}
+
+#[entry_point]
+pub fn reply(deps: DepsMut, env: Env, reply: Reply) -> Result<Response, ContractError> {
+    execute::reply(deps, env, reply)
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    execute::query(deps, env, msg)
+}
+
+#[entry_point]
+pub fn migrate(_deps: DepsMut, _env: Env, _msg: MigrateMsg) -> StdResult<Response> {
+    Ok(Response::new())
+}
